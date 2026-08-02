14c13ac01e71cdb0fa9fd2faafb13b22a4ad3285
nvasilevbg <nvasilevbg@gmail.com>
2026-08-02 13:51:57 +0300

    ux: Facebook link moved to bottom (discreet), only Google CTA in hero

--- otzivi.php
+++ otzivi.php
@@ -19,9 +19,7 @@ require __DIR__ . '/inc/header.php';
         <svg viewBox="0 0 24 24" width="22" height="22" aria-hidden="true"><path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/><path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/><path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z"/><path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/></svg>
         Оставете отзив в Google
       </a>
-      <?php if (!empty($s['facebook'])): ?>
-      <a class="btn btn-ghost reviewFbBtn" href="<?= e($s['facebook']) ?>" target="_blank" rel="noopener noreferrer"><?= icon('facebook') ?> Facebook</a>
-      <?php endif; ?>
+
     </div>
   </div>
 </section>
@@ -63,4 +61,14 @@ require __DIR__ . '/inc/header.php';
     </div>
   </div>
 </section>
+    </div>
+  </div>
+</section>
+<?php if (!empty($s['facebook'])): ?>
+<section class="section" style="padding-top:0">
+  <div class="container" style="max-width:640px;text-align:center">
+    <a class="reviewFb" href="<?= e($s['facebook']) ?>" target="_blank" rel="noopener noreferrer"><?= icon('facebook') ?> Или ни намерете във Facebook</a>
+  </div>
+</section>
+<?php endif; ?>
 <?php require __DIR__ . '/inc/footer.php'; ?>
