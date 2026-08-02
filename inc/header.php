7b717b89875f11314f36fcdf9807b2e495b94365
nvasilevbg <nvasilevbg@gmail.com>
2026-08-02 13:54:43 +0300

    fix: center tips heading + container, list stays left-aligned

--- inc/header.php
+++ inc/header.php
@@ -20,8 +20,8 @@ $current = $GLOBALS['path'] ?? '';
 <meta name="description" content="<?= e($desc ?? 'Монтаж и демонтаж на мебели в Добрич, Балчик, Варна и околността.') ?>">
 <link rel="icon" href="/assets/media/icon.svg" type="image/svg+xml">
 <link rel="stylesheet" href="/assets/fonts/fonts.css?v=1">
-<link rel="preload" href="/assets/css/style.css?v=11" as="style">
-<link rel="stylesheet" href="/assets/css/style.css?v=11">
+<link rel="preload" href="/assets/css/style.css?v=12" as="style">
+<link rel="stylesheet" href="/assets/css/style.css?v=12">
 </head>
 <body>
 <?php if (!empty($GLOBALS['minimal_header'])): ?>
